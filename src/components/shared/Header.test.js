2eebebf2760766790be8c4beb6ae6542256d2dca
Kalvin <kalvinhart@hotmail.co.uk>
2021-05-16 17:19:06 +0100

    Header component complete and test passed.

--- src/components/shared/Header.test.js
+++ src/components/shared/Header.test.js
@@ -1,9 +1,17 @@
 import Header from "./Header";
+import { ThemeProvider } from "styled-components";
+import { themes } from "../../theme";
 import { render, screen } from "@testing-library/react";
 
 describe("<Header/>", () => {
+  // TEST =========================================================
   test("Header is rendered", () => {
-    render(<Header />);
+    const themeToggle = jest.fn();
+    render(
+      <ThemeProvider theme={themes["light"]}>
+        <Header themeToggle={themeToggle} />
+      </ThemeProvider>
+    );
     expect(screen.getByTestId("header")).toBeInTheDocument();
   });
 });
