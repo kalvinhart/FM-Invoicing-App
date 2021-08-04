7e8071bd3461a51255b12f8dd5f7a360658a5b72
Kalvin <kalvinhart@hotmail.co.uk>
2021-08-04 22:44:17 +0100

    Removed redundant useState and React import.

--- src/components/shared/Checkbox/Checkbox.js
+++ src/components/shared/Checkbox/Checkbox.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import { useContext } from "react";
 import PropTypes from "prop-types";
 import { FiltersContext } from "../../../store/FiltersContext";
 import {
